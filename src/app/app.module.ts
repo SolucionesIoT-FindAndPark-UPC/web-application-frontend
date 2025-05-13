4a2418d1d2763737e5b8802a438882a35c14e8ef
Luis Herrera <u202218227@upc.edu.pe>
2025-05-13 11:37:48 -0500

    feat: parking-lot.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import {HeaderComponent} from './shared/components/header/header.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,10 +16,11 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    AppRoutingModule,
     RouterModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/parking-lots/pages/parking-lots/parking-lots.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-parking-lots',
+  imports: [HeaderComponent],
+  templateUrl: './parking-lots.component.html',
+  standalone: true,
+  styleUrl: './parking-lots.component.css'
+})
+export class ParkingLotsComponent {
+
+}
