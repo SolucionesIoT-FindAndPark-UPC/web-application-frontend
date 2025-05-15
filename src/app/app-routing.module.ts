b3613669b1da0493ac9d59d9cf6722d4247a3886
Luis Herrera <u202218227@upc.edu.pe>
2025-05-15 14:04:19 -0500

    feat: added statistics and profile.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './public/components/page-not-found/page-not-found.component';
 import { FeedComponent } from './dashboard/components/feed/feed.component';
 import {ParkingLotsComponent} from './parking-lots/pages/parking-lots/parking-lots.component';
+import {StatisticsComponent} from './statistics/page/statistics/statistics.component';
+import {ProfileComponent} from './profiles/pages/profile/profile.component';
 
 const routes: Routes = [
   {path: 'dashboard-page', component: FeedComponent},
   {path: 'parking-lots', component: ParkingLotsComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: '', redirectTo: 'dashboard-page', pathMatch: 'full' },
   {path: '**', component: PageNotFoundComponent }
 ];
