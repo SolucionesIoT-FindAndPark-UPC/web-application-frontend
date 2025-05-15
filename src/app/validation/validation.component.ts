0af2b8e47813eaac177d5f3a2f90efd064b859df
Eric Cuevas <u202112766@upc.edu.pe>
2025-05-15 02:23:40 -0500

    feat: validation screen

--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-validation',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './validation.component.html',
   styleUrl: './validation.component.css'
 })
@@ -25,16 +24,12 @@ export class ValidationComponent implements OnInit {
       address1: ['', Validators.required],
       address2: [''],
       additionalInfo: [''],
-      // Aquí podrías añadir más campos según necesites
     });
   }
 
   onSubmit(): void {
     if (this.subscriptionForm.valid) {
       console.log('Form submitted:', this.subscriptionForm.value);
-      // Aquí iría la lógica para enviar los datos al servidor
-      // Por ejemplo:
-      // this.mallService.subscribeMall(this.subscriptionForm.value).subscribe(...);
     }
   }
 }
